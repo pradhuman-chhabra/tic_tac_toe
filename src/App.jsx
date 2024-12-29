2328088b037bbd57eb1354321c26c467f7e4d339
Pradhuman Chhabra <72680645+pradhuman-chhabra@users.noreply.github.com>
2024-12-29 14:46:33 +0530

    added the abolity to press on sqaures and have them print x when their state is chnaged using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,53 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // value stores the value and setValue is a function that can be used to change the value. The null passed to useState is used as the initial value for this state variable, so value here starts off equal to null.
 
-// function Square() {
-//   const [value, setValue] = useState(null);
-
-function Square({ value }) {
+// Button jab click hoga toh onClick method se handleclick Funtion call hoga, handClcik will set the the value to X for "value" variable
+// Yeh Value naam ka ek variable declare kiya hai jo useState hook use karta hain, us hook se click hone pe "value" ki value X set ho jaati hain
+function Square() {
+  const [value, setValue] = useState(null);
   function handleClick() {
-    console.log("Button Clicked");
+    setValue('X');
   }
   // all the function declarations and js code should be written before return , because what ever is in return statement returns the chunk of the code from the function
   // button component ki on click (prop)property will call the function handleClick whenever Clicked 
   return (
     <button className="square" onClick={handleClick}>
-
       {value}
     </button>
   );
 }
 
+// function Square({ value }) {
+//   function handleClick() {
+//     console.log("Button Clicked");
+//   }
+//   // all the function declarations and js code should be written before return , because what ever is in return statement returns the chunk of the code from the function
+//   // button component ki on click (prop)property will call the function handleClick whenever Clicked 
+//   return (
+//     <button className="square" onClick={handleClick}>
+//       {value}
+//     </button>
+//   );
+// }
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
+        <Square />
         {/* ps jahan component use hota hain wohin se prop pass kiya jaata hain */}
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
